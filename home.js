9bc6e4e47d61275c25f624f4f2e74123b13e319e
Foster Brown <fosterbrown@Fosters-iMac.nyc.rr.com>
2019-08-10 01:07:36 -0400

    added buttons to trigger details

---
+++ home.js
@@ -0,0 +1,15 @@
+function hideOrShowDetails(id) { 
+    element = document.getElementById(id)
+
+    if (element.style.visibility == "visible"){
+        console.log("True")
+        element.style.visibility="hidden";
+    }
+
+    else if(element.style.visibility == "hidden"){
+        console.log(element.style.visibility)
+        element.style.visibility="visible";
+    }
+    
+}
+
